db4f58475fb118214e79c67946581258c5a58e67
Alex <oleksii.petrochenko.pz.2019@lpnu.ua>
2021-10-22 01:10:02 +0300

    [LARAVEL-1] users setup

--- app/Models/ProductItem.php
+++ app/Models/ProductItem.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class ProductItem extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];  
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\InfoController;
+use App\Http\Controllers\AdminPanelController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,3 +32,17 @@ Route::prefix('shop')->group(function(){
     Route::get('/', [ShopController::class, 'home'])->name('shop');
     Route::get('/{id}', [ShopController::class, 'singleItem'])->whereNumber('id')->name('singleItem');
 });
+
+Route::prefix('admin')->group(function(){
+    Route::get('/', [AdminPanelController::class, 'main'])->name('admin-panel')->middleware('auth:admin');
+    Route::post('/add-new-product', [AdminPanelController::class, 'addNewProduct'])->name('add-new-product')->middleware('auth:admin');
+});
+
+Auth::routes();
+Route::get('/login/admin', [LoginController::class, 'showAdminLoginForm']);
+Route::get('/register/admin', [RegisterController::class, 'showAdminRegisterForm']);
+Route::post('/login/admin', [LoginController::class, 'adminLogin']);
+Route::post('/register/admin', [RegisterController::class, 'createAdmin']);
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
