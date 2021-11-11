a49a8288761b2797f15b064689b4b13242370095
[Oleksii Petrochenko] <[oleksiipetrochenko@gmail.com]>
2021-11-11 23:08:29 +0200

    [LARAVEL-4]

---
+++ database/factories/ProductItemFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ProductItem;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductItemFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ProductItem::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
